86b09fd143b104efe8ae9c68b05c2a965f0458da
Bolaji Olajide <cooproton@gmail.com>
2021-03-12 01:32:07 +0100

    add mongoose model

---
+++ api-two/models.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require('mongoose');
+
+
+const ReplySchema = new Schema(
+  {
+    intent: {
+      type: Schema.Types.String,
+      unique: true,
+      required: true,
+      trim: true
+    },
+    response: {
+      type: Schema.Types.String,
+      required: true,
+      trim: true
+    }
+  },
+  { timestamps: true },
+  {
+    toObject: { virtuals: true },
+    toJSON: { virtuals: true }
+  },
+);
+
+module.exports = model('replies', ReplySchema);
---
+++ api-two/routes.js
@@ -0,0 +1,1 @@
+const respond = require('../utils/respond');
