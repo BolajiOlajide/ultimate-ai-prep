d1f457b689d44002d95cbaa16960ea0b0f8c0756
Bolaji Olajide <cooproton@gmail.com>
2021-03-13 21:15:30 +0100

    add test script for api one

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+run_test_api_one() {
+  cd api-one
+  yarn test
+  cd -
+}
+
 run_test_api_two() {
   cd api-two
   yarn test
   cd -
 }
 
-run_test_api_two
+run_test_api_one && run_test_api_two
